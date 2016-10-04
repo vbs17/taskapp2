ce819b49218fe90102859bdbc8797764b8734ff9
kei ikeuchi <sister@sister.local>
2016-10-04 19:59:55 +0900

    done

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -1,23 +1,19 @@
-//
-//  ViewController.swift
-//  taskapp
-//
-//  Created by kei ikeuchi on 2016/10/04.
-//  Copyright © 2016年 mycompany. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
